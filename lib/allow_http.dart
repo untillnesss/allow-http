7d056e2dec3e9cf0f61008a42b224950337e7057
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2022-06-05 20:33:37 +0700

    init

---
+++ lib/allow_http.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library allow_http;
+
+export 'src/allow_http_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
